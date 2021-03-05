4972d3ceb358f9ca6fb5f6c7034a21eee465e940
Marcin Przytuła <mprzytula94@gmail.com>
2021-03-05 08:28:05 +0100

    reset

---
+++ src/pages/HomePage.js
@@ -0,0 +1,34 @@
+import React from "react";
+import Article from "../components/Article";
+
+const articles = [
+  {
+    id: 1,
+    title: "Czym jest teoria strun?",
+    author: "Jan Nowak",
+    text:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type",
+  },
+  {
+    id: 1,
+    title: "Czym jest paradoks fermiego?",
+    author: "Ania Polanska",
+    text:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type",
+  },
+  {
+    id: 1,
+    title: "Czym jest materia i ciemna energia?",
+    author: "Kamil Kozak",
+    text:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type",
+  },
+];
+const HomePage = () => {
+  const artList = articles.map(article => (
+    <Article key={article.id} {...article} />
+  ));
+  return <div className="home">{artList}</div>;
+};
+
+export default HomePage;
